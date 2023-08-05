063d4a107584e7ff01b1409b6356b70a14b0937a
EMailina <erik55.malina@gmail.com>
2023-08-05 11:18:21 +0200

    trying to set up material ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule
   ],
-  providers: [],
+  providers: [MatToolbarModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
